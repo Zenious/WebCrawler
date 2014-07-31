065ecb926eff3ab71b9b0f1fe624d4440f0c3b29
Zenious <brain_basher@hotmail.com>
2014-07-31 16:44:42 +0800

    added option for choosing number of threads for downloading and processing

--- src/webcrawler/GUIv2.java
+++ src/webcrawler/GUIv2.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package webcrawler;
 
 import java.awt.Component;
@@ -11,9 +10,14 @@ import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JProgressBar;
 import javax.swing.JTable;
 import javax.swing.SwingWorker;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
@@ -27,14 +31,16 @@ import threadhandle.ExecutorHandler;
  * @author Zheng Wei
  */
 public class GUIv2 extends javax.swing.JFrame {
+
     private int emptyRow = 0;
     public static List<String> seeds = Collections.synchronizedList(new ArrayList<String>());
     public static ExecutorHandler ex;
     public static List<Page> donePages = Collections.synchronizedList(new ArrayList<Page>());
-    public static final int numberOfURLs = 15; 
+    public static final int numberOfURLs = 15;
     public static DefaultTableModel dtm;
-    
-        
+    private int noOfDownload = 5; 
+    private int noOfProcess = 5;
+
     /**
      * Creates new form GUIv2
      */
@@ -74,6 +80,11 @@ public class GUIv2 extends javax.swing.JFrame {
         seedPanel = new javax.swing.JPanel();
         seedInput = new javax.swing.JTextField();
         seedLabel = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+        downloadThreadMenu = new javax.swing.JMenu();
+        processingThreadMenu = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("WebCrawler - Java");
@@ -178,6 +189,58 @@ public class GUIv2 extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
+        jMenu1.setText("File");
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+
+        downloadThreadMenu.setText("Number of Download Threads");
+        jMenu2.add(downloadThreadMenu);
+        downloadThreadBG = new ButtonGroup();
+        ArrayList<JCheckBoxMenuItem> downloadThreadChoices = new ArrayList<JCheckBoxMenuItem>();
+        for(int i = 1; i<11; i++){
+            downloadThreadChoices.add(new JCheckBoxMenuItem(String.valueOf(i)));
+        }
+        for(JCheckBoxMenuItem checkbox : downloadThreadChoices){
+            if (checkbox.getText().equals(String.valueOf(noOfDownload))){
+                checkbox.setSelected(true);
+            }
+            checkbox.addChangeListener(new ChangeListener() {
+                public void stateChanged(ChangeEvent changeEvent){
+                    JCheckBoxMenuItem checkbox = (JCheckBoxMenuItem)changeEvent.getSource();
+                    if (checkbox.isSelected()) noOfDownload = Integer.parseInt(checkbox.getText());
+                }
+            });
+            downloadThreadMenu.add(checkbox);
+            downloadThreadBG.add(checkbox);
+        }
+
+        processingThreadMenu.setText("Number of Processing Threads");
+        jMenu2.add(processingThreadMenu);
+        ButtonGroup processThreadBG = new ButtonGroup();
+
+        ArrayList<JCheckBoxMenuItem> processThreadChoices = new ArrayList<JCheckBoxMenuItem>();
+        for(int i = 1; i<11; i++){
+            processThreadChoices.add(new JCheckBoxMenuItem(String.valueOf(i)));
+        }
+        for(JCheckBoxMenuItem checkbox : processThreadChoices){
+            if (checkbox.getText().equals(String.valueOf(noOfProcess))){
+                checkbox.setSelected(true);
+            }
+            checkbox.addChangeListener(new ChangeListener() {
+                public void stateChanged(ChangeEvent changeEvent){
+                    JCheckBoxMenuItem checkbox = (JCheckBoxMenuItem)changeEvent.getSource();
+                    if (checkbox.isSelected()) noOfProcess = Integer.parseInt(checkbox.getText());
+                }
+            });
+            processingThreadMenu.add(checkbox);
+            processThreadBG.add(checkbox);
+        }
+
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -208,15 +271,26 @@ public class GUIv2 extends javax.swing.JFrame {
 
     private void addButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addButtonActionPerformed
         // TODO add your handling code here:
-        seeds.add(seedInput.getText());
-        dtm = (DefaultTableModel) pageTable.getModel();
-        dtm.addRow(new Object[][] {null, null, null, null});
-        while (dtm.getValueAt(emptyRow, 0) != null){
-            emptyRow++;
+        if (seedInput.getText().matches("http://(\\w+\\.)+(\\w+)")) {
+            if (seeds.contains(seedInput.getText())) {
+                JOptionPane.showMessageDialog(pageScrollPane, "Url already inside list!", "Duplicated url", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                seeds.add(seedInput.getText());
+                dtm = (DefaultTableModel) pageTable.getModel();
+                dtm.addRow(new Object[][]{null, null, null, null});
+                while (dtm.getValueAt(emptyRow, 0) != null) {
+                    emptyRow++;
+                }
+                dtm.setValueAt(seedInput.getText(), emptyRow, 0);
+                dtm.setValueAt(0, emptyRow, 1);
+                dtm.setValueAt("Queued", emptyRow, 2);
+            }
+            seedInput.setText("Enter Website...");
+        } else if (seedInput.getText().equals("Enter Website...")) {
+            JOptionPane.showMessageDialog(pageScrollPane, "Please Enter a url.", "No Url", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(pageScrollPane, "Please Enter a valid url.", "Invalid url", JOptionPane.INFORMATION_MESSAGE);
         }
-        dtm.setValueAt(seedInput.getText(), emptyRow, 0);
-        dtm.setValueAt(0, emptyRow, 1);
-        dtm.setValueAt("Queued", emptyRow, 2);
     }//GEN-LAST:event_addButtonActionPerformed
 
     private void seedInputActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_seedInputActionPerformed
@@ -225,30 +299,29 @@ public class GUIv2 extends javax.swing.JFrame {
 
     private void submitButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_submitButtonActionPerformed
         // TODO add your handling code here:
-            System.out.println(seeds.size());
-            ex = new ExecutorHandler(5,5,seeds);
-            ex.start();
+        ex = new ExecutorHandler(noOfDownload, noOfProcess, seeds);
+        ex.start();
 
             //DefaultTableModel dtm = (DefaultTableModel) jTable1.getModel();
-          //  for (int i = 0; i < dtm.getRowCount(); i++) {
-          //    if(dtm.getValueAt(i, 2).toString().equals("Queued")){
-          //      UrlHanderWorker uhw = new UrlHanderWorker(dtm, jTextField1.getText(), 0);
-          //      uhw.execute();
-            //    }
-            //}
+        //  for (int i = 0; i < dtm.getRowCount(); i++) {
+        //    if(dtm.getValueAt(i, 2).toString().equals("Queued")){
+        //      UrlHanderWorker uhw = new UrlHanderWorker(dtm, jTextField1.getText(), 0);
+        //      uhw.execute();
+        //    }
+        //}
 
     }//GEN-LAST:event_submitButtonActionPerformed
 
     private void seedInputFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_seedInputFocusGained
         // TODO add your handling code here:
-        if (seedInput.getText().equals("Enter Website...")){
+        if (seedInput.getText().equals("Enter Website...")) {
             seedInput.setText("");
         }
     }//GEN-LAST:event_seedInputFocusGained
 
     private void seedInputFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_seedInputFocusLost
         // TODO add your handling code here:
-        if (seedInput.getText().isEmpty()){
+        if (seedInput.getText().isEmpty()) {
             seedInput.setText("Enter Website...");
         }
     }//GEN-LAST:event_seedInputFocusLost
@@ -287,31 +360,37 @@ public class GUIv2 extends javax.swing.JFrame {
             }
         });
     }
-    
-    public class ProgressBarRender extends JProgressBar implements TableCellRenderer{
+
+    public class ProgressBarRender extends JProgressBar implements TableCellRenderer {
 
         @Override
         public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
             int progress = 0;
-            if (value instanceof Float){
-                progress = Math.round((float)value /100);
+            if (value instanceof Float) {
+                progress = Math.round((float) value / 100);
                 setValue(progress);
-            }else if (value instanceof Integer){
-                progress = (int)value;
+            } else if (value instanceof Integer) {
+                progress = (int) value;
                 setValue(progress);
-            }else if (value instanceof  String){
-                setString((String)value);       
+            } else if (value instanceof String) {
+                setString((String) value);
             }
             setStringPainted(true);
             return this;
         }
     }
-    
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton addButton;
     private javax.swing.JPanel buttonPanel;
+    private javax.swing.JMenu downloadThreadMenu;
+    private javax.swing.ButtonGroup downloadThreadBG;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JScrollPane pageScrollPane;
     private javax.swing.JTable pageTable;
+    private javax.swing.JMenu processingThreadMenu;
     private javax.swing.JTextField seedInput;
     private javax.swing.JLabel seedLabel;
     private javax.swing.JPanel seedPanel;
